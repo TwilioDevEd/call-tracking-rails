800e922683234c9030408e8ecc716ae48a6dc3f5
Agustin Camino <agustin.camino@gmail.com>
2015-09-11 08:19:53 -0500

    rename transformation method

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,17 +1,17 @@
 class StatisticsController < ApplicationController
   def leads_by_source
     lead_sources = LeadSource.count_leads
-    render json: format_for_d3(lead_sources)
+    render json: format_for_chart(lead_sources)
   end
 
   def leads_by_city
     leads = Lead.count_by_city
-    render json: format_for_d3(leads)
+    render json: format_for_chart(leads)
   end
 
   private
 
-  def format_for_d3(values)
+  def format_for_chart(values)
     values.map do |label, value|
       {
         label: label,
